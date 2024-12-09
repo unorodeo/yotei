b55927ee7b5e3f51433c913dc9d53ec33ec6940f
enoabasi <enoabasidream@gmail.com>
2024-12-09 17:26:16 +0100

    refac: default page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,11 +1,7 @@
-import { Panel } from "@/components/core/panel";
-import { Preview } from "@/components/core/preview";
-
 export default function Yotei() {
   return (
-    <section>
-      <Panel />
-      <Preview />
-    </section>
+    <>
+      themes
+    </>
   );
 }
